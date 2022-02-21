cb50acf4af7fe02d095caad85cfa41ec56ba3e70
Carlos Mauricio <50898095+CMDeveloper97@users.noreply.github.com>
2022-02-20 19:30:22 -0700

    Update firebase.js

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBSIpg3WHe4cf5E8_--_7WfV9THcMwRPOU",
-    authDomain: "tenedores-f63d3.firebaseapp.com",
-    databaseURL: "https://tenedores-f63d3.firebaseio.com",
-    projectId: "tenedores-f63d3",
-    storageBucket: "tenedores-f63d3.appspot.com",
-    messagingSenderId: "308257435212",
-    appId: "1:308257435212:web:fa9f3aa39a5a8a1e2ccee9"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   // Initialize Firebase
